aeca5e846d997ae0aa7251031cfb0440c888e881
Chaos0103 <lyt1228@naver.com>
2023-05-10 17:49:02 +0900

    build: axios 설정

--- src/main.js
+++ src/main.js
@@ -3,7 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import axios from 'axios';
+
 Vue.config.productionTip = false
+Vue.prototype.$eventBus = new Vue();
+Vue.prototype.$axios = axios
 
 new Vue({
   router,
