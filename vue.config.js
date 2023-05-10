d45ef8de7f5b7675792c3856463d35128c0e54a4
Chaos0103 <lyt1228@naver.com>
2023-05-10 14:24:26 +0900

    build: 포트번호 변경 및 빌드 경로 설정

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  outputDir: "../Tourmates-Server/src/main/resources/static",
+
+  devServer: {
+    proxy: {
+      '/': {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+      }
+    }
+  }
 })
