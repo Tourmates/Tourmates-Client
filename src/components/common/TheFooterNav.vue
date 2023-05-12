8542a38596dba289dc334739ba31f24656408876
Chaos0103 <lyt1228@naver.com>
2023-05-12 17:26:20 +0900

    feat: header, footer 구현

---
+++ src/components/common/TheFooterNav.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <b-navbar toggleable="lg" type="dark" variant="info">
+            <b-navbar-brand>&copy; 2023 Company, Inc</b-navbar-brand>
+            <b-collapse id="nav-collapse" is-nav>
+                <b-navbar-nav class="ml-auto">
+                    <b-nav-item to="/notices">공지사항</b-nav-item>
+                    <b-nav-item to="#">이용약관</b-nav-item>
+                    <b-nav-item to="#">개인정보처리방침</b-nav-item>
+                    <b-nav-item to="#">FAQs</b-nav-item>
+                </b-navbar-nav>
+            </b-collapse>
+        </b-navbar>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "TheFooterNav",
+    components: {},
+    data() {
+        return {
+            message: "",
+        };
+    },
+    created() {
+    },
+    methods: {},
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/components/common/TheHeaderNav.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <b-navbar toggleable="lg" type="dark" variant="info">
+            <b-navbar-brand to="/">NavBar</b-navbar-brand>
+            <b-collapse id="nav-collapse" is-nav>
+                <b-navbar-nav class="ml-auto">
+                    <b-nav-item to="/attractions">지역별여행지</b-nav-item>
+                    <b-nav-item to="/tripPlans">나의여행계획</b-nav-item>
+                    <b-nav-item to="/hotPlaces">핫플자랑하기</b-nav-item>
+                    <b-nav-item to="/boards">자유게시판</b-nav-item>
+                    <b-nav-item to="/login">로그인</b-nav-item>
+                    <b-nav-item to="/join">회원가입</b-nav-item>
+                </b-navbar-nav>
+            </b-collapse>
+        </b-navbar>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "TheHeaderNav",
+    components: {},
+    data() {
+        return {
+            message: "",
+        };
+    },
+    created() {
+    },
+    methods: {},
+};
+</script>
+
+<style>
+
+</style>
