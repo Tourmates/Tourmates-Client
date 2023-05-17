0ba258fbc4af6b1b7b6160799b8fd32683b17a79
chaos0103 <lyt1228@naver.com>
2023-05-17 17:17:40 +0900

    feat: 문의사항 컴포넌트 적용

--- src/components/qna/QnaList.vue
+++ src/components/qna/QnaList.vue
@@ -1,10 +1,15 @@
 <template>
-  <div>
-
-  </div>
+    <div class="container">
+        <h1>문의사항</h1>
+        <router-view/>
+        <qna-paging/>
+    </div>
 </template>
 <script>
+import QnaPaging from "@/components/qna/list/QnaPaging.vue";
+
 export default {
     name: "QnaList",
+    components: {QnaPaging},
 }
 </script>
