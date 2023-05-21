d765e460d454886649534095b838bc5f2c1eaa92
정유빈 <youbeen2798@naver.com>
2023-05-21 17:42:25 +0900

    fix: MyPage.vue 게시물 UI 수정 완료

--- src/components/member/MyBoards.vue
+++ src/components/member/MyBoards.vue
@@ -1,66 +1,51 @@
 <template>
-    <section class='container'>
-    <table class="table mt-3">
-        <tbody class="table-group-divider">
-        <div v-for = "board in boards" :key = "board"> 
-            <tr>
-                <!-- <th class='align-middle text-center'>
-                        {{board.tag}}
-                </th> -->
-                <td>
-                    <div>
-                        <a href='${root}/articles/${article.articleId}'>
-                                {{board.title}}
-                        </a>
-                    </div>
-                    <div>
-                        {{board.nickname}} | {{board.createdDate}}
-                    </div>
-                </td>
-                <td class='align-bottom'>
-                    조회수 {{board.hit}}
-                </td>
-            </tr>
+    <div>
+        <div v-if="boards.length === 0">등록된 게시물이 없습니다.</div>
+        <div v-else>
+            <table class="table mt-3 container-sm">
+                <colgroup>
+                    <col :style="{width: '10%'}">
+                    <col :style="{width: '70%'}">
+                    <col :style="{width: '20%'}">
+                </colgroup>
+                <tbody class="table-group-divider">
+                <board-list-row
+                v-for="(board, index) in boards"
+                :key="`${index}_boards`"
+                :boardId="board.boardId"
+                :title="board.title"
+                :nickname="board.nickname"
+                :createdDate="board.createdDate"
+                :hit="board.hit"
+                />
+                </tbody>
+            </table>
         </div>
-        </tbody>
-    </table>
-    <div class="d-flex justify-content-center">
-        <nav aria-label="Page navigation example">
-            <ul class="pagination">
-                <li class="page-item">
-                    <a class="page-link" href="#" aria-label="Previous">
-                        <span aria-hidden="true">&laquo;</span>
-                    </a>
-                </li>
-                <li class="page-item"><a class="page-link" href="#">1</a></li>
-                <li class="page-item"><a class="page-link" href="#">2</a></li>
-                <li class="page-item"><a class="page-link" href="#">3</a></li>
-                <li class="page-item">
-                    <a class="page-link" href="#" aria-label="Next">
-                        <span aria-hidden="true">&raquo;</span>
-                    </a>
-                </li>
-            </ul>
-        </nav>
-    </div>
-    <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-        <a href="${root}/articles/register" class="btn btn-primary" type="button">글쓰기</a>
-    </div>
-</section>
+    </div> 
 </template>
 
 <script>
+
+import BoardListRow from "@/components/board/list/BoardListRow.vue";
 import axios from "axios";
 
 export default{
     name: "MyBoards",
+    components: {BoardListRow},
+
     data(){
         return {
             boards: [],
-        };
+            pageLimit: 10,
+        }
     },
     created(){
         this.initComponent();
+    },
+    watch: {
+        '$route.query': function () {
+            this.initComponent();
+        }
     },
     methods: {
         initComponent(){
@@ -70,7 +55,7 @@ export default{
                 this.$route.query.pageNumber = 10;
             }
 
-            const API_URL = `http://localhost:8080/my/boards?pageNumber=1`;
+            const API_URL = `http://localhost:8080/my/boards?pageNumber=${this.$route.query.pageNumber - this.pageLimit}`;
 
             console.log("hihiihihi");
             axios.get(API_URL)
@@ -78,11 +63,10 @@ export default{
                     console.log("response");
                     console.log(response);
                     this.boards = response.data.data;
+                    this.page = response.data.page;
                     console.log("boards"); 
                     console.log(this.boards);
                 });
-
-//            const API_URL = `http://localhost:8080/my/boards?pageNumber=${this.$route.query.pageNumber - this.pageLimit}`;
 
         }
     }
