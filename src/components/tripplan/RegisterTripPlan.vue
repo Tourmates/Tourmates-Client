27d3a2082f199353ded6266b4cf03ef4868b042f
chaos0103 <lyt1228@naver.com>
2023-05-22 21:50:07 +0900

    feat: 관광지 정보 조회 뷰 구현

--- src/components/tripplan/RegisterTripPlan.vue
+++ src/components/tripplan/RegisterTripPlan.vue
@@ -1,78 +1,67 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-6">
-                지도 예정
-            </div>
-            <div class="col-3">
-                <h3>table1</h3>
-                <draggable
-                        class="list-group"
-                        :list="list1"
-                        group="people"
-                        @change="log"
-                        itemKey="name"
+    <div class="row">
+        <div class="col-3">
+            <h3>Draggable 1</h3>
+            <draggable
+                    class="list-group"
+                    :list="list1"
+                    group="people"
+                    @change="log"
+            >
+                <div
+                        class="list-group-item"
+                        v-for="element in list1"
+                        :key="element.name"
                 >
-                    <template #item="{ element, index }">
-                        <div class="list-group-item">{{ element.name }} {{ index }}</div>
-                    </template>
-                </draggable>
-            </div>
-            <div class="col-3">
-                <h3>table2</h3>
-                <draggable
-                        class="list-group"
-                        :list="list2"
-                        group="people"
-                        @change="log"
-                        itemKey="name"
+                    {{ element.name }}
+                </div>
+            </draggable>
+        </div>
+        <div class="col-3">
+            <h3>Draggable 2</h3>
+            <draggable
+                    class="list-group"
+                    :list="list2"
+                    group="people"
+                    @change="log"
+            >
+                <div
+                        class="list-group-item"
+                        v-for="element in list2"
+                        :key="element.name"
                 >
-                    <template #item="{ element, index }">
-                        <div class="list-group-item">{{ element.name }} {{ index }}</div>
-                    </template>
-                </draggable>
-            </div>
+                    {{ element.name }}
+                </div>
+            </draggable>
         </div>
     </div>
 </template>
 <script>
-import {draggable} from "vuedraggable";
+import draggable from "@/vuedraggable";
 
 export default {
     name: "RegisterTripPlan",
-    components: {
-        draggable,
-    },
+    components: {draggable},
     data() {
         return {
             list1: [
-                {name: "John", id: 1},
-                {name: "Joao", id: 2},
-                {name: "Jean", id: 3},
-                {name: "Gerard", id: 4}
+                {name: "name1", id: 1},
+                {name: "name2", id: 2},
+                {name: "name3", id: 3},
+                {name: "name4", id: 4},
             ],
             list2: [
-                {name: "Juan", id: 5},
-                {name: "Edgard", id: 6},
-                {name: "Johnson", id: 7}
+                {name: "test1", id: 5},
+                {name: "test2", id: 6},
+                {name: "test3", id: 7},
             ]
         };
     },
     methods: {
-        add: function () {
-            this.list.push({name: "Juan"});
-        },
-        replace: function () {
-            this.list = [{name: "Edgard"}];
-        },
-        clone: function (el) {
-            return {
-                name: el.name + " cloned"
-            };
-        },
         log: function (evt) {
             window.console.log(evt);
         }
-    },
+    }
+
 }
 </script>
