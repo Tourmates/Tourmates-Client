105d3ddf2118566e13742e165807b7b67f9a5559
youbeen2798 <youbeen2798@naver.com>
2023-05-23 15:32:09 +0900

    feat: MyPage.vue 나의 친구 등록하기 버튼 추가

--- src/components/member/MyPage.vue
+++ src/components/member/MyPage.vue
@@ -18,6 +18,19 @@
           </div>
         </div>
       </div>
+         <div class="col" onclick="location.href='/my/friends'">
+            <div class="card h-100 border-0 shadow">
+                <div class="card-body">
+                   <div class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3 rounded-circle">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-person-fill m-2" viewBox="0 0 16 16">
+                        <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
+                      </svg>
+                   </div>
+                   <h5 class="card-title">나의 친구 등록</h5>
+                     <p class="card-text">친구와 함께 여행 계획 정보를 공유할 수 있습니다.</p>
+                 </div>
+             </div>
+         </div>
       <div class="col" onclick="location.href='/my/tripplans'">
         <div class="card h-100 border-0 shadow">
           <div class="card-body">
