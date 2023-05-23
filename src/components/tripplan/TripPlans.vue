2a804d7eda1b5fd9d56b56b4c3b4df51a2bf4d1e
youbeen2798 <youbeen2798@naver.com>
2023-05-23 15:00:45 +0900

    feat: TripPlans 구현 완료

--- src/components/tripplan/TripPlans.vue
+++ src/components/tripplan/TripPlans.vue
@@ -1,8 +1,24 @@
 <template>
-  <div>
-      <router-view/>
-      <trip-plan-paging/>
-  </div>
+    <div class="container">
+        <h1 class="text-center">여행계획</h1>
+        <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+            <div>
+                <select class="form-select" v-model="type">
+                    <option value="0">제목</option>
+                    <option value="1">작성자</option>
+                    <option value="2">관광지</option>
+                </select>
+            </div>
+            <div>
+                <input type="text" class="form-control" id="keyword" name="keyword" placeholder="검색어" v-model="keyword">
+            </div>
+            <div>
+                <button class="btn btn-primary" type="button">검색</button>
+            </div>
+        </div>
+        <router-view/>
+        <trip-plan-paging/>
+    </div>
 </template>
 <script>
 import TripPlanPaging from "@/components/tripplan/list/TripPlanPaging.vue";
@@ -12,11 +28,10 @@ export default {
     components: {TripPlanPaging},
     data() {
         return {
-
+            keyword: "",
+            type: 0
         };
     },
-    methods: {
-
-    },
+    methods: {},
 }
 </script>
