4dea0cdf1b6468fe0c405eb64d8d4e430b3e1de8
youbeen2798 <youbeen2798@naver.com>
2023-05-23 13:57:39 +0900

    feat: index.js DetailTripPlan 추가

--- src/router/index.js
+++ src/router/index.js
@@ -39,6 +39,7 @@ import TripPlanList from "@/components/tripplan/list/TripPlanList.vue";
 import RegisterTripPlan from "@/components/tripplan/RegisterTripPlan.vue";
 import WithDrawal from "@/components/member/WithDrawal.vue";
 import MyHotPlaceList from "@/components/member/MyHotPlaceList.vue";
+import DetailTripPlan from "@/components/tripplan/DetailTripPlan.vue";
 
 Vue.use(VueRouter)
 
@@ -252,6 +253,11 @@ const routes = [
         path: 'register',
         name: 'registerTripPlan',
         component: RegisterTripPlan,
+      },
+      {
+        path: ':tripPlanId',
+        name: 'detailTripPlan',
+        component: DetailTripPlan,
       }
     ]
   }
