ad8c4b968b025dde6eadac66afd17739ffe5a0d5
youbeen2798 <youbeen2798@naver.com>
2023-05-23 15:00:03 +0900

    feat: TripPlanListLow 기능 구현 완료

--- src/components/tripplan/list/TripPlanListRow.vue
+++ src/components/tripplan/list/TripPlanListRow.vue
@@ -1,13 +1,23 @@
 <template>
-  <div>
-
-  </div>
+    <tr>
+        <th class="text-center">{{ tripPlanId }}</th>
+        <td>
+            <router-link :to="`/tripPlans/${tripPlanId}`">{{ title }}</router-link>
+        </td>
+        <td class="text-center">{{ nickname }}</td>
+        <td class="text-center">{{ hit }}</td>
+        <td class="text-center">{{ createdDate }}</td>
+    </tr>
 </template>
 <script>
 export default {
     name: "TripPlanListRow",
     props: {
-
+        tripPlanId: {type: Number},
+        title: {type: String},
+        nickname: {type: String},
+        hit: {type: Number},
+        createdDate: {type: String},
     },
 }
 </script>
