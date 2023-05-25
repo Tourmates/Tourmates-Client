3674c805ced1f74404510eb83b4be5154665f88f
youbeen2798 <youbeen2798@naver.com>
2023-05-25 15:13:30 +0900

    feat: MyTripPlanPaging 처리

---
+++ src/views/MyTripPlans.vue
@@ -0,0 +1,26 @@
+<template>
+        <div class="container">
+                <div class="mt-5 mb-5 text-center">
+                        <h3>나의 여행계획</h3>
+                </div>
+                <hr/>
+                <br/>
+                <router-view/>
+                <my-trip-plan-paging/>
+        </div>
+</template>
+<script>
+import MyTripPlanPaging from "@/components/member/TripPlan/list/MyTripPlanPaging.vue";
+
+export default {
+    name: "MyTripPlans",
+    components: {MyTripPlanPaging},
+    data() {
+        return {
+            keyword: "",
+            type: 0
+        };
+    },
+    methods: {},
+}
+</script>
